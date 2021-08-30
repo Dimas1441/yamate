1c061eebb17365fc2808e4f374680c7e00001b13
Dimas1441 <89393904+Dimas1441@users.noreply.github.com>
2021-08-30 18:40:31 +0700

    Update and rename panel-domain.sh to domain.sh

--- 2/domain.sh
+++ 2/domain.sh
@@ -7,12 +7,12 @@ bd='\e[1m'
 MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
 clear
-figlet PANEL SSH | lolcat -d 5
+figlet PANEL DOMAIN | lolcat -d 5
 echo -e ""
 echo -e "\e[1;32m═════════════════════════════════════════\e[m" | lolcat
 echo -e "          \e[1;31m\e[1;31m═[\e[mSSH & OpenVPN Menu\e[1;31m]═\e[m" 
 echo -e "\e[1;32m═════════════════════════════════════════\e[m" | lolcat
-echo -e   " $bl ║\e[m$bd 1$bl]\e[m$bd Add Subdomain Host For VPS                             "
+echo -e   " $bl ║\e[m$bd 1$bl]\e[m$bd Add Subdomain Host For VPS"
 echo -e   " $bl ║\e[m$bd 2$bl]\e[m$bd Add ID Cloudflare"
 echo -e   " $bl ║\e[m$bd 3$bl]\e[m$bd Cloudflare Add-Ons" 
 echo -e   " $bl ║\e[m$bd 4$bl]\e[m$bd Pointing BUG"
@@ -21,9 +21,9 @@ echo -e "\e[1;32m═════════════════════
 echo -e " x)   MENU        \e[m" | lolcat
 echo -e "\e[1;32m══════════════════════════════════════════\e[m" | lolcat
 echo -e ""
-read -p "     Please Input Number  [1-4 or x] :  "  panel-domain
+read -p "     Please Input Number  [1-4 or x] :  "  domain
 echo -e ""
-case $panel-domain in
+case $domain in
 1)
 add-host
 ;;
@@ -38,8 +38,12 @@ cfh
 ;;
 5)
 certv2ray
+;;
 x)
 exit
+;;
+x)
+menu
 ;;
 *)
 echo "Please enter an correct number"
