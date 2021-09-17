5af47171b62d7304089a9c00139aaba2024b6695
Dimas1441 <89393904+Dimas1441@users.noreply.github.com>
2021-09-17 13:26:55 +0700

    Update log.sh

--- 2/log.sh
+++ 2/log.sh
@@ -10,7 +10,7 @@ echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                : 902, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - WebSocket/WS SSL        : 443"  | tee -a log-install.txt
+echo "   - WebSocket/WS SSL        : 2053"  | tee -a log-install.txt
 echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
 echo "   - WebSocket/WS Ovpn       : 2086"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
